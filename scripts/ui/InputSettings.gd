433dd5886eedbd24f41c92c95fe3f5919bc498cc
SinethT <103297183+SinethT@users.noreply.github.com>
2024-08-27 20:30:14 +1200

    Code formatted

--- scripts/ui/InputSettings.gd
+++ scripts/ui/InputSettings.gd
@@ -6,8 +6,13 @@ var is_remapping = false
 var action_to_remap = null
 var remapping_button = null
 
-var input_actions = {"go_up": "Jump", "go_right": "Move Right", "go_left": "Move Left", "go_down": "Fall", "shoot": "Shoot"}
-
+var input_actions = {
+	"go_up": "Jump",
+	"go_right": "Move Right",
+	"go_left": "Move Left",
+	"go_down": "Fall",
+	"shoot": "Shoot"
+}
 
 @onready var input_button_scene = preload("res://scenes/ui/InputButton.tscn")
 @onready var action_list = $Panel/MarginContainer/VBoxContainer/ScrollContainer/ActionList
@@ -15,25 +20,26 @@ var input_actions = {"go_up": "Jump", "go_right": "Move Right", "go_left": "Move
 
 func _ready():
 	_create_action_list()
-	
+
+
 func _create_action_list():
 	InputMap.load_from_project_settings()
 	for item in action_list.get_children():
 		item.queue_free()
-	
+
 	for action in input_actions:
 		var button = input_button_scene.instantiate()
 		var action_label = button.find_child("LabelAction")
 		var input_label = button.find_child("LabelInput")
-		
+
 		action_label.text = input_actions[action]
-		
+
 		var events = InputMap.action_get_events(action)
 		if events.size() > 0:
 			input_label.text = events[0].as_text().trim_suffix(" (Physical)")
 		else:
 			input_label.text = ""
-		
+
 		action_list.add_child(button)
 		button.pressed.connect(_on_input_button_pressed.bind(button, action))
 
@@ -45,25 +51,24 @@ func _on_input_button_pressed(button, action):
 		remapping_button = button
 		button.find_child("LabelInput").text = "Press key to bind..."
 
+
 func _input(event):
 	if is_remapping:
-		if (
-			event is InputEventKey or
-			(event is InputEventMouseButton and event.pressed)
-		):
+		if event is InputEventKey or (event is InputEventMouseButton and event.pressed):
 			# Turn double click into single click
 			if event is InputEventMouseButton and event.double_click:
 				event.double_click = false
-			
+
 			InputMap.action_erase_events(action_to_remap)
 			InputMap.action_add_event(action_to_remap, event)
 			_update_action_list(remapping_button, event)
-			
+
 			is_remapping = false
 			action_to_remap = null
 			remapping_button = null
-			
+
 			accept_event()
+
 
 func _update_action_list(button, event):
 	button.find_child("LabelInput").text = event.as_text().trim_suffix(" (Physical)")
